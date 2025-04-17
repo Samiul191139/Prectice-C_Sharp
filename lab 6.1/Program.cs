f6f9ca6b1a15525f64e01ed12974b95685ebcd7e
Sami <samikarim191139@gmail.com>
2025-04-18 02:53:49 +0600

    done lab report 6

---
+++ lab 6.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace lab_6
+{
+    class SimpleCalculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Simple Division Calculator");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter the first number: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter the second number: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+                    if (num2 == 0)
+                    {
+                        throw new DivideByZeroException();
+                    }
+
+                    double result = DivideNumbers(num1, num2);
+                    Console.WriteLine("Result: " + result);
+                    break;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("You cannot divide by zero.\n");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input.\n");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong.\n");
+                }
+            }
+        }
+
+        static double DivideNumbers(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
+}
